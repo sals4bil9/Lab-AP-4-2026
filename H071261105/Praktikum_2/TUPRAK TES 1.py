1ca343a029211d56bf0cb75249b3322a126c703f
.git <EMAIL_GITHUB>
2026-09-18 14:54:15 +0800

    feat:Praktikum_2

---
+++ H071261105/Praktikum_2/TUPRAK TES 1.py
@@ -0,0 +1,11 @@
+level_pedas = int(input("Masukkan persentase cabai: "))
+if level_pedas >= 0 and level_pedas <= 10:
+    print("Level Aman")
+elif level_pedas >= 11 and level_pedas <= 40:
+    print("Level Sedang")
+elif level_pedas >= 41 and level_pedas <= 70:
+    print("Level Pedas")
+elif level_pedas > 70 and level_pedas <= 100:
+    print("Level Ekstrem")
+else:
+    print("Invalid")
